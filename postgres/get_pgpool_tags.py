98db8a659be194f291eaa9b3aa0299d00dd4ecbc
cam <cam@cams-MacBook-Air.local>
2026-04-14 15:39:34 +0700

    fix(harbor): add ingressClassName nginx and fix hostname

--- postgres/get_pgpool_tags.py
+++
@@ -1,9 +0,0 @@
-import json, urllib.request
-url = "https://hub.docker.com/v2/repositories/bitnami/pgpool/tags?page_size=10"
-try:
-    req = urllib.request.urlopen(url)
-    res = json.loads(req.read().decode('utf-8'))
-    for tag in res.get('results', []):
-        print(tag['name'])
-except Exception as e:
-    print("Error:", e)
